6aabdf5800f3d0b96c742b4c33e26e25a68eb6ac
Jeff Johnson <jeffjohnson9046hotmail.com>
2012-08-29 15:44:51 -0700

    Added test to verify links on pages.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
+  #------------------- TEST ROUTES -------------------#
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector("title", :content => "Home")
@@ -23,6 +24,25 @@ describe "LayoutLinks" do
 
   it "should have a Sign Up page at '/signup'" do
     get '/signup'
+    response.should have_selector("title", :content => "Sign Up")
+  end
+
+  #------------------- TEST LINKS -------------------#
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector("title", :content => "About")
+
+    click_link "Help"
+    response.should have_selector("title", :content => "Help")
+
+    click_link "Contact"
+    response.should have_selector("title", :content => "Contact")
+
+    click_link "Home"
+    response.should have_selector("title", :content => "Home")
+
+    click_link "Sign up now!"
     response.should have_selector("title", :content => "Sign Up")
   end
 end
