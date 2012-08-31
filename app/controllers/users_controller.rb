498ef8347570d0991b6dd66b2c5fffab3008f7a6
Jeff Johnson <jeffjohnson9046hotmail.com>
2012-08-31 14:51:51 -0700

    Added _error_messages partial view and styling to support it.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
+  # POST /users
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # handle the save...
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
+  end
+
+  # GET /users/new
   def new
     @user = User.new
     @title = "Sign Up"
   end
 
+  # GET /users/1
   def show
     @user = User.find(params[:id])
     @title = @user.name
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,5 @@
 # The Factory Girl gem will allow us to simulate a User object with default values.  Basically, Factory Girl will
 # create the mock objects for us, just like most other mocking frameworks.
-#Factory.define :user do |user|
-#  user.name                   "Darth Vader"
-#  user.email                  "bigdaddyv@deathstar.com"
-#  user.password               "foobar"
-#  user.password_confirmation  "foobar"
-#end
-
-
 FactoryGirl.define do
   factory :user do
     name "Darth Vader"
